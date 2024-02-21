26bc03eb5cec7a8bde70043562cdadf57feffad7
Mia Coupland <miacoupland@btinternet.com>
2024-02-21 18:58:42 +0000

    Add enums

---
+++ src/app/tracker/models/grade.enum.ts
@@ -0,0 +1,14 @@
+export enum GradeEnum {
+  FG = 1,
+  SD,
+  EG,
+  HG,
+  PBandaiHG,
+  RG,
+  PBandaiRG,
+  MG,
+  PBandaiMG,
+  PG,
+  MSM,
+  UC
+}
---
+++ src/app/tracker/models/universe.enum.ts
@@ -0,0 +1,13 @@
+export enum UniverseEnum {
+  UniversalCentury = 1,
+  FutureCentury,
+  AfterColonyMarsCentury,
+  AfterWar,
+  CorrectCentury,
+  CosmicEra,
+  AnnoDomini,
+  AdvancedGeneration,
+  RegildCentury,
+  PostDisaster,
+  AdStella,
+}
