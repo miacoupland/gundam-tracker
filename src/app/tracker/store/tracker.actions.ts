564782f55afa2929a2038f8f4354cb42165e6e4b
Mia Coupland <miacoupland@btinternet.com>
2024-02-22 21:04:43 +0000

    Add tracker actions

--- src/app/tracker/store/tracker.actions.ts
+++ src/app/tracker/store/tracker.actions.ts
@@ -1,45 +1,8 @@
-import { Action } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 import { Model } from '../models/model.model';
 
-export const ADD_GUNDAM = '[Tracker] Add Gundam';
-export const DELETE_GUNDAM = '[Tracker] Delete Gundam';
-export const UPDATE_GUNDAM = '[Tracker] Update Gundam';
-export const START_EDIT = '[Tracker] Start Editing';
-export const STOP_EDIT = '[Tracker] Stop Editing';
-
-export class AddGundam implements Action {
-  readonly type = ADD_GUNDAM;
-
-  constructor(public payload: Model) {}
-}
-
-export class DeleteGundam implements Action {
-  readonly type = DELETE_GUNDAM;
-
-  constructor(public payload: Model) {}
-}
-
-export class UpdateGundam implements Action {
-  readonly type = UPDATE_GUNDAM;
-
-  constructor(public payload: Model) {}
-}
-
-export class StartEdit implements Action {
-  readonly type = START_EDIT;
-
-  constructor(public payload: number) {}
-}
-
-export class StopEdit implements Action {
-  readonly type = STOP_EDIT;
-
-  constructor() {}
-}
-
-export type TrackerActions =
-  | AddGundam
-  | DeleteGundam
-  | UpdateGundam
-  | StartEdit
-  | StopEdit;
+export const addGundam = createAction('[Tracker] Add Gundam', props<Model>());
+export const deleteGundam = createAction('[Tracker] Delete Gundam', props<Model>());
+export const updateGundam = createAction('[Tracker] Update Gundam', props<Model>());
+export const startEdit = createAction('[Tracker] Start Edit', props<{ index: number }>());
+export const stopEdit = createAction('[Tracker] Stop Edit');
