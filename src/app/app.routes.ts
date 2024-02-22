f9d14c92a24f253df65d83a0a672f16840173b25
Mia Coupland <miacoupland@btinternet.com>
2024-02-22 20:58:51 +0000

    Set up basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { TrackerComponent } from './tracker/tracker.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'tracker',
+        component: TrackerComponent,
+      }
+    ]
+  }
+];
