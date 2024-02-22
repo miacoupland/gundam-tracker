844b20adc3e6a81052fcb5992d2ffc809d6b1e4a
Mia Coupland <miacoupland@btinternet.com>
2024-02-22 20:59:55 +0000

    Set up basic routes

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
