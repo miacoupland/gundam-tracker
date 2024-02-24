dfe64f16813a5ac2e97ce197f3e44c19ee0eb139
Mia Coupland <miacoupland@btinternet.com>
2024-02-24 15:17:57 +0000

    Add tracker reducer

--- src/app/tracker/store/tracker.reducer.ts
+++ src/app/tracker/store/tracker.reducer.ts
@@ -1,64 +1,63 @@
-import { Model } from '../models/model.model';
 import * as TrackerActions from './tracker.actions';
+import { TrackerDetails } from '../models/tracker-details.model';
+import { createReducer, on } from '@ngrx/store';
 
-export interface State {
-  models: Model[];
-  editedModel: Model | null;
-  editedModelIndex: number;
-}
-
-const initialState = {
+const initialTrackerState: TrackerDetails = {
   models: [],
   editedModel: null,
   editedModelIndex: -1,
 };
 
-export function trackerReducer(
-  state: State = initialState,
-  action: TrackerActions.TrackerActions
-) {
-  switch (action.type) {
-    case TrackerActions.ADD_GUNDAM:
-      return {
-        ...state,
-        models: [...state.models, action.payload],
-      };
-    case TrackerActions.DELETE_GUNDAM:
-      return {
-        ...state,
-        models: state.models.filter((md, mdIndex) => {
-          return mdIndex !== state.editedModelIndex;
-        }),
-        editedModelIndex: -1,
-        editedModel: null,
-      };
-    case TrackerActions.UPDATE_GUNDAM:
-      const model = state.models[state.editedModelIndex];
-      const updatedModel = {
-        ...model,
-        ...action.payload,
-      };
-      const updatedModels = [...state.models];
-      updatedModels[state.editedModelIndex] = updatedModel;
-      return {
-        ...state,
-        models: updatedModels,
-        editedModelIndex: -1,
-        editedModel: null,
-      };
-    case TrackerActions.START_EDIT:
-      return {
-        ...state,
-        editedModelIndex: action.payload,
-        editedModel: { ...state.models[action.payload] },
-      };
-    case TrackerActions.STOP_EDIT:
-      return {
-        ...state,
-        editedModel: null,
-        editedModelIndex: -1,
-      };
-    default:
-      return state;
-  }
-}
+export const trackerReducer = createReducer(
+  initialTrackerState,
+  on(
+    TrackerActions.addGundam,
+    (state, model): TrackerDetails => ({
+      ...state,
+      models: [...state.models, model],
+    })
+  ),
+  on(
+    TrackerActions.deleteGundam,
+    (state, model): TrackerDetails => ({
+      ...state,
+      models: state.models.filter((md, mdIndex) => {
+        return mdIndex !== state.editedModelIndex;
+      }),
+      editedModelIndex: -1,
+      editedModel: null,
+    })
+  ),
+
+  on(TrackerActions.updateGundam, (state, model): TrackerDetails => {
+    const selectedModel = state.models[state.editedModelIndex];
+    const updatedModel = {
+      ...selectedModel,
+      ...model,
+    };
+    const updatedModels = [...state.models];
+    updatedModels[state.editedModelIndex] = updatedModel;
+    return {
+      ...state,
+      models: updatedModels,
+      editedModelIndex: -1,
+      editedModel: null,
+    };
+  }),
+  on(
+    TrackerActions.startEdit,
+    (state, { index }): TrackerDetails => ({
+      ...state,
+      editedModelIndex: index,
+      editedModel: { ...state.models[index] },
+    })
+  ),
+  on(
+    TrackerActions.stopEdit,
+    (state): TrackerDetails => ({
+      ...state,
+      editedModel: null,
+      editedModelIndex: -1,
+    })
+  )
+);
